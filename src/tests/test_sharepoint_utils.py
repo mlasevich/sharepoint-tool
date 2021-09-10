27df473332f5a384f27d326e5fbb7302ca31d03b
Michael "M3" Lasevich <michael@legrig.com>
2021-09-09 18:24:08 -0700

    fix unit test

--- src/tests/test_sharepoint_utils.py
+++ src/tests/test_sharepoint_utils.py
@@ -20,9 +20,9 @@ class SharepointUtilsTests(unittest.TestCase):
         tests = [
             ('', ''),
             (' ', '%20'),
-            ('$ & < > ? ; # : = , " \' ~ + %',
+            ('$ & < > ? ; # : = , " \' + %',
              '%24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22'
-             '%20%27%20~%20%2B%20%25')
+             '%20%27%20%2B%20%25')
         ]
         for source, expected in tests:
             actual = utils.urlencode(source)
